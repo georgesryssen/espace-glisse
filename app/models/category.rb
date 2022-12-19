e6e068020ff77ed10d7759656b19ec66ecdc145f
georges ryssen <georges.ryssen@gmail.com>
2022-12-19 17:45:50 +0100

    progrès en cours sur les forms et devise

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,15 @@
 class Category < ApplicationRecord
   has_many :lessons
+  CATEGORY = [
+    {
+      field: 'Eau',
+      sports: ['Surf','Kitesurf', 'Bodyboard', 'Paddle', 'Windsurf', 'Wingsurf', 'Wingfoil', 'Skimboard',
+        'Paddlesurf', 'Wakeboard', 'Ski nautique']
+    },
+    {
+      field: 'Neige',
+      sports: ['Ski', 'Snowboard', 'Ski de fond', 'Snowkite', 'Airboard', 'Raquettes', 'Snowscoot']
+    }
+  ].freeze
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :bookings
-  belongs_to :school
+  belongs_to :school, optional: true
 end
